0ee47435b3e7a92e9a3f3b5dd8bc36514048e77a
Slav win <Sl-aj@yandex.ua>
2019-05-21 14:12:48 +0300

    site portfolio finish

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,12 @@ class Blog(models.Model):
     pup_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pup_date_pretty(self):
+        return self.pup_date.strftime('%b %e %Y')
